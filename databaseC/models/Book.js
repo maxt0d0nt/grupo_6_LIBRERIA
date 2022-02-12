d808ace9fa6a616c95b70c9a469744e464f56745
Nicolas Portesio <nicolas.portesio@42i.co>
2022-02-12 13:50:12 -0300

    .
    Signed-off-by: Nicolas Portesio <nicolas.portesio@42i.co>

--- databaseC/models/Book.js
+++ databaseC/models/Book.js
@@ -3,15 +3,6 @@ const sequelize = require ('../db')
 
 class Book extends Model {}
 
-// "id": 1,
-// "name": "Ficciones",
-// "author": "Jose Luis Borges",
-// "path": "/img/uploads/products/ficcionesBorges.jpg",
-// "description": "Las ediciones actuales de Ficciones reúnen todos los relatos publicados en la edición de 1956 (que agrega tres relatos a la versión de 1944) y siguen dividiendo el libro en dos partes: El jardín de los senderos que se bifurcan y Artificios.     ",
-// "price": "1450",
-// "literatureCategory": "action",
-// "ecomerceCategory": "promotion"
-
 Book.init({
     name: DataTypes.STRING,
     img: DataTypes.STRING,
