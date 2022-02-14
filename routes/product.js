f0f8046caa99e5d5bcb61e034c606c588823a61f
Nicolas Portesio <nicolas.portesio@42i.co>
2022-02-14 12:46:58 -0300

    .
    Signed-off-by: Nicolas Portesio <nicolas.portesio@42i.co>

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const productController = require('../controllers/productController');
-const productApiController = require('../controllers/api/apiProduct');
 const upload = require('../middlewares/upload');
 const authMiddleware = require('../middlewares/authMiddleware');
 
