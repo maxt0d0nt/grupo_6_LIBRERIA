06b5f79e2872df28fcb83eddb77d0a64797fb025
Nicolas Portesio <nicolas.portesio@42i.co>
2022-02-16 19:42:39 -0300

    .
    Signed-off-by: Nicolas Portesio <nicolas.portesio@42i.co>

--- databaseC/asociations.js
+++ databaseC/asociations.js
@@ -5,9 +5,10 @@ const Ecommerce = require('./models/Ecommerce')
 const Literature = require('./models/Literature')
 const BookAuthors = require('./models/BookAuthors')
 
-Book.belongsToMany(Author,{as:'author', through: BookAuthors,foreignKey:"author_id" })
-Author.belongsToMany(Book,{ through: BookAuthors, foreignKey:"book_id" })
-
+Author.hasMany(Book,{as:"books",foreignKey:"author_id"})
+Book.belongsTo(Author,{as:"author",foreignKey:"author_id"})
+// Book.belongsToMany(Author,{as:'author', through: BookAuthors,foreignKey:"author_id" })
+// Author.belongsToMany(Book,{ through: BookAuthors, foreignKey:"book_id" })
 
 Ecommerce.hasMany(Book,{as:"books",foreignKey:"ecommerce_id"})
 Book.belongsTo(Ecommerce,{as:"ecommerce_category",foreignKey:"ecommerce_id"})
